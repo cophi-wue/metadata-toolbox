9cdc1f21c864e854f4e1ef580126b3342f747b7e
severinsimmler <severin.simmler@posteo.de>
2017-12-12 14:18:42 +0100

    Add header in utils.py

---
+++ metadata_toolbox/utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Heading
+*******
+About this module...
+
+
+Contents
+********
+    * :func:`example_function()`: About this function...
+"""
